95815267cd4fc0dd7f4d0571a34f35ad43e73a63
Arwa119 <marwahussain1409@gmail.com>
2025-04-18 20:51:52 +0500

    Implement row insertion functionality and enhance error handling in the database REPL

--- DB/main.cpp
+++ DB/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 #include "input_handler.h"
+#include "row.h"
 using namespace std;
 
 // ------- REPL -----------
@@ -21,12 +23,34 @@ int main()
         switch (command)
         {
         case CommandType::INSERT:
-            cout << "This is where will insert.\n";
+        {
+            stringstream ss(input);
+            string keyword;
+            int id;
+            string name, email;
+            ss >> keyword >> id >> name >> email;
+            if (ss.fail() || name.length() > COLUMN_NAME_SIZE || email.length() > COLUMN_EMAIL_SIZE)
+            {
+                cout << "Syntax error or field too long. Usage : insert <id> <name> <email>\n ";
+                break;
+            }
+
+            Row row;
+            row.id = id;
+            strncpy(row.name, name.c_str(), COLUMN_NAME_SIZE);
+            strncpy(row.email, email.c_str(), COLUMN_EMAIL_SIZE);
+
+            cout << "Row Inserted: \n";
+            print_row(row);
             break;
+        }
         case CommandType::SELECT:
+        {
             cout << "This is where will select.\n";
             break;
+        }
         case CommandType::UNKOWN:
+        default:
             cout << "Unrecognized command: " << input << "\n";
             break;
         }
