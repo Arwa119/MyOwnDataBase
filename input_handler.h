7fed07ee0460e989047fd043fd3bd8d19893a6f6
khadijafassih <144409536+khadijafassih@users.noreply.github.com>
2025-05-09 17:13:14 +0500

    input_handler.h

--- input_handler.h
+++ input_handler.h
@@ -3,20 +3,39 @@
 
 #include <string>
 #include <iostream>
-using namespace std;
+#include <algorithm>
+#include <cctype>
 
 enum class CommandType{
     INSERT,
     SELECT,
+    DELETE,
+    BTREE_CMD,
     UNKOWN
 };
 
-CommandType parse_command(const string& input){
-    if(input.find("insert") == 0){
+CommandType parse_command(const std::string& input){
+    auto first_char = std::find_if(input.begin(), input.end(), [](unsigned char ch) {
+        return !std::isspace(ch);
+    });
+
+    if (first_char == input.end()) {
+        return CommandType::UNKOWN;
+    }
+
+    std::string trimmed_input(first_char, input.end());
+
+    if(trimmed_input.find("insert") == 0){
         return CommandType::INSERT;
     }
-    else if(input == "select"){
+    else if(trimmed_input.find("select") == 0){
         return CommandType::SELECT;
+    }
+    else if(trimmed_input.find("delete") == 0){
+        return CommandType::DELETE;
+    }
+    else if(trimmed_input == ".btree"){
+        return CommandType::BTREE_CMD;
     }
     else{
         return CommandType::UNKOWN;
